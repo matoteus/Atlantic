e3934933b89788816c58cc4a83e7b896d7ebaa9c
matoteus <macavalcantep1@gmail.com>
2018-05-25 16:25:45 -0300

    Update BulletScript.cs

--- BulletScript.cs
+++ BulletScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
 
-	private Rigidbody2D rg;
-	private AudioSource aud;
+	private Rigidbody2D rg; // RECEBE O COMPONENTE RIGIDBODY DA BALA
+	private AudioSource aud; // RECEBE O COMPONENTE DE AUDIO
 
-	public AudioClip shoot;
+	public AudioClip shoot; // RECEBE O SOM DE TIRO
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +15,8 @@ public class BulletScript : MonoBehaviour {
 		aud = GetComponent<AudioSource> ();
 		rg = GetComponent<Rigidbody2D> ();
 
-		rg.velocity = new Vector3 (GameManager.instance_d.x ,GameManager.instance_d.y, 0);
-		aud.PlayOneShot (shoot);
+		rg.velocity = new Vector3 (GameManager.instance_d.x ,GameManager.instance_d.y, 0); // INSTANCIA A BALA DE ACORDO COM OS VALORES DO GAMEMANAGER
+		aud.PlayOneShot (shoot); // EXECUTA O AUDIO DE TIRO 
 		
 	}
 	
